23b9e7c52c1d521147e330bdd9ee4613d01772bb
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-12-30 18:16:23 +0700

    [pull] master from BianSepang:master #2

--- userbot/utils/progress.py
+++ userbot/utils/progress.py
@@ -23,7 +23,7 @@ from .exceptions import CancelProcess
 
 
 async def progress(
-    current, total, gdrive, start, prog_type,
+    current, total, event, start, prog_type,
     file_name=None, is_cancelled=False
 ):
     now = time.time()
@@ -31,7 +31,7 @@ async def progress(
     if is_cancelled is True:
         raise CancelProcess
 
-    if round(diff % 10.00) == 0 or current == total:
+    if round(diff % 15.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff)
@@ -56,5 +56,5 @@ async def progress(
             f"`ETA` -> {time_formatter(eta)}\n"
             f"`Duration` -> {time_formatter(elapsed_time)}"
         )
-        await gdrive.edit(f"`{prog_type}`\n\n"
-                          f"`Status`\n{tmp}")
+        await event.edit(f"`{prog_type}`\n\n"
+                         f"`Status`\n{tmp}")
